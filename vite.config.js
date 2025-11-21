33fb0d146f65b8067221200165d2c8db834876b7
Luciano Gonçalves <lhgllm39@gmail.com>
2025-11-20 22:47:14 -0300

    FINAL FIX: Relative path build 404 errors on GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-// Adicione esta linha:
-import { defineConfig } from 'vite'; 
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // ⚙️ Configuração do caminho base para o GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  // Você corrigiu esta linha para caminhos relativos:
-  base: './', 
+  base: "/ping-pong/", // 👈 coloque o nome exato do seu repositório aqui
 });
