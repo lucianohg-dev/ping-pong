2f6c7db018d8511dbffdf7703259b4c0126a5370
lucianohg-dev <133520629+lucianohg-dev@users.noreply.github.com>
2025-11-20 21:53:50 -0300

    Update vite.config.js
    Renomeação da base no vite.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // ⚙️ Configuração do caminho base para o GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: "/ping-pong/", // 👈 coloque o nome exato do seu repositório aqui
+  base: "/ping-pong-clone/", // 👈 coloque o nome exato do seu repositório aqui
 });
