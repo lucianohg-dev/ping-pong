b64721f88ab43486d6ddad2eb6f23d226547b655
Luciano Gonçalves <lhgllm39@gmail.com>
2025-11-20 23:17:43 -0300

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// ⚙️ Configuração do caminho base para o GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: "/ping-pong/", // 👈 coloque o nome exato do seu repositório aqui
-});
